fcd77d331b3e723bbf4f4cfd2a1fc4fbad9eba9e
Ilham <48025760+ipirm@users.noreply.github.com>
2019-05-04 22:41:43 +0400

    Js update

--- main.js
+++ main.js
@@ -5,4 +5,20 @@
                 width: "toggle"
             });
         });
+
+        $(".card-main__item").click(function(){
+          $(".card-main__item").removeClass( "card-main__active" );
+      $( this ).toggleClass( "card-main__active" );
     });
+
+    $(".menu-tools-item").click(function(){
+      $(".menu-tools-item").removeClass( "active-tool" );
+  $( this ).toggleClass( "active-tool" );
+  });
+
+  $(".sidebar-title").click(function(){
+    $(".sidebar-title").removeClass( "active-title" );
+$( this ).toggleClass( "active-title" );
+ $( this ).next( ".js-togle__btn" ).slideToggle( "slow" );
+});
+      });
